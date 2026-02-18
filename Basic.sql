fecdf5d292f8fc20b3a5647a59f973399b7c44cc
Lakshay Kumawat <work.lakshayk@gmail.com>
2026-02-18 15:20:40 +0530

    Add files via upload

---
+++ Basic.sql
@@ -0,0 +1,40 @@
+
+show databases;
+use mysql;
+
+create database lakshay;
+show databases;
+
+use lakshay;
+
+-- to see the output select statement
+select database();
+
+show tables;  -- display tables
+create table first(salary int, age int);
+show tables;
+
+-- display information of a table
+describe first;
+
+/*
+-- to see databases
+show databases;
+
+-- to print current database
+select database();
+
+-- to work with data
+use database;
+
+-- to see tables
+show tables;
+
+-- create table
+create table_name(column datatype);
+
+-- table (description) -- to see the column name and datatype
+describe table_name;
+*/
+
+
